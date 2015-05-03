7dd21a68ce2bd3569491a345dc46bb1511be5336
Ian Macalinao <me@ian.pw>
2015-05-03 18:11:36 -0500

    Add field for entering a number

--- app/src/main/java/pw/ian/ieee754converter/MainActivity.java
+++ app/src/main/java/pw/ian/ieee754converter/MainActivity.java
@@ -4,16 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends ActionBarActivity {
+     ToggleButton x32;
+     ToggleButton x64;
+     boolean is32Bit = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        x32 = (ToggleButton) findViewById(R.id.x32);
+        x64 = (ToggleButton) findViewById(R.id.x64);
+        x32.setChecked(is32Bit);
+
+        x32.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                set32Bit(true);
+            }
+        });
+        x64.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                set32Bit(false);
+            }
+        });
     }
 
+    public void set32Bit(boolean value) {
+        final ToggleButton x32 = (ToggleButton) findViewById(R.id.x32);
+        final ToggleButton x64 = (ToggleButton) findViewById(R.id.x64);
+        x32.setChecked(value);
+        x64.setChecked(!value);
+        is32Bit = value;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
