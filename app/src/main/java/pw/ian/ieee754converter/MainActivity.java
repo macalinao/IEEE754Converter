62208292b88e23f6c29f04e6279410e65230452d
Ian Macalinao <me@ian.pw>
2015-05-03 21:38:17 -0500

    Add about menu

--- app/src/main/java/pw/ian/ieee754converter/MainActivity.java
+++ app/src/main/java/pw/ian/ieee754converter/MainActivity.java
@@ -152,7 +152,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.about) {
             return true;
         }
 
